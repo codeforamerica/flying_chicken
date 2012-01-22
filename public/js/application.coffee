f1f9e21a1005705e635c81df79901edeef97c21b
Zach Williams <hey@zachwill.com>
2012-01-22 08:48:43 -0800

    Add iframe when audio is playing

--- public/js/application.coffee
+++ public/js/application.coffee
@@ -5,12 +5,24 @@ zappa.run ->
     console.log "welcome: #{@data.time}"
 
   @on newcommit: ->
-      # play sound
-      # play commit via google translate
-      # show latest commit info
-      index = @data.index
-      audio = =>
-        $('div#audio').append "<audio src='#{@data.sound_url}' autoplay controls></audio>"
-        $('audio').on 'ended', (event) -> $(this).remove()
-      _.delay(audio, index * 250)
-      $('div#message').html "#{@data.commit.author.username} committed to #{@data.repository.name} <br/>"
+    # play sound
+    # play commit via google translate
+    index = @data.index
+    [username, repo] = [@data.commit.author.username, @data.repository.name]
+    message = "#{username} committed to #{repo}"
+    audio = =>
+      $('div#audio').append "<audio src='#{@data.sound_url}' autoplay controls></audio>"
+      $('audio').on('playing', (event) ->
+        self = $(this)
+        parent = self.parent()
+        encoded = encodeURIComponent(message.replace(/_/g, ' '))
+        translate = "http://translate.google.com/translate_tts?q=#{encoded}"
+        iframe = $("<iframe src='#{translate}'></iframe>").appendTo(parent)
+        remove_iframe = -> iframe.remove()
+        _.delay(remove_iframe, 10000)
+      ).on('ended', (event) ->
+        $(this).remove()
+      )
+    _.delay(audio, index * 350)
+    # show latest commit info
+    $('div#message').html message
--- public/js/application.js
+++ public/js/application.js
@@ -9,17 +9,30 @@
     });
     return this.on({
       newcommit: function() {
-        var audio, index,
+        var audio, index, message, repo, username, _ref,
           _this = this;
         index = this.data.index;
+        _ref = [this.data.commit.author.username, this.data.repository.name], username = _ref[0], repo = _ref[1];
+        message = "" + username + " committed to " + repo;
         audio = function() {
           $('div#audio').append("<audio src='" + _this.data.sound_url + "' autoplay controls></audio>");
-          return $('audio').on('ended', function(event) {
+          return $('audio').on('playing', function(event) {
+            var encoded, iframe, parent, remove_iframe, self, translate;
+            self = $(this);
+            parent = self.parent();
+            encoded = encodeURIComponent(message.replace(/_/g, ' '));
+            translate = "http://translate.google.com/translate_tts?q=" + encoded;
+            iframe = $("<iframe src='" + translate + "'></iframe>").appendTo(parent);
+            remove_iframe = function() {
+              return iframe.remove();
+            };
+            return _.delay(remove_iframe, 10000);
+          }).on('ended', function(event) {
             return $(this).remove();
           });
         };
-        _.delay(audio, index * 250);
-        return $('div#message').html("" + this.data.commit.author.username + " committed to " + this.data.repository.name + " <br/>");
+        _.delay(audio, index * 350);
+        return $('div#message').html(message);
       }
     });
   });
